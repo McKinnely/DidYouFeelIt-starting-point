e8834b4d0f94c8eb55cf4823bb04c4cbe3d5dd73
McKinnely <McKinnely.bentley@gmail.com>
2017-02-09 20:50:16 -0600

    Getting Solidly Back On Droid Life Projects....

--- app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
+++ app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
@@ -13,6 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+//Back on Driod Life!
+
+
 package com.example.android.didyoufeelit;
 
 import android.os.Bundle;
